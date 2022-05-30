e355c652968e3f20b5fef5d5084f21c48545821e
masterismael <106500446+masterismael@users.noreply.github.com>
2022-05-29 19:55:21 -0700

    Cambios

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require ("gulp-sass")(require('sass'));
 
 
 function css(done){
-    src("src/scss/app.scss")
+    src("src/scss/**/*.scss")
     .pipe(sass())
     .pipe(dest("build/css"))
 
@@ -13,7 +13,7 @@ function css(done){
 
 
 function dev(done){
-    watch("src/scss/app.scss",css)
+    watch("src/scss/**/*.scss",css)
 
     done();
 }
